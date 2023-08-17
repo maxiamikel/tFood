db3204b745209097586354d7d9ca2be85ce96f0d
maxiamikel <maxloversist@gmail.com>
2023-08-17 08:01:57 -0300

    Inicializar o projeto tFood criando o back

---
+++ tFood-back/src/main/java/com/maxi/tFoodback/TFoodBackApplication.java
@@ -0,0 +1,13 @@
+package com.maxi.tFoodback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TFoodBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TFoodBackApplication.class, args);
+	}
+
+}
---
+++ tFood-back/src/test/java/com/maxi/tFoodback/TFoodBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxi.tFoodback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TFoodBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
